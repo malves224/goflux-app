daae4325f95781d0314832201f7693212680a454
Matheus Alves <malves224@gmail.com>
2022-04-30 20:46:55 -0300

    Feat (controllers): add middleware de validação de schema.

--- app/backend/src/controllers/ShipperController.ts
+++ app/backend/src/controllers/ShipperController.ts
@@ -1,12 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
 import Shipper from '../database/models/shipper';
 import { IShipper } from '../interfaces/Shiper';
+import ShipperAndConveyorsSchema from '../schema/ShipperAndConveyors';
 import ShipperService from '../services/ShipperService';
 import GenericController from './GenericControllers';
 
 class ShipperController extends GenericController<IShipper, Shipper> {
-  constructor(route: string, service = new ShipperService()) {
+  constructor(
+    route: string, 
+    public schema = new ShipperAndConveyorsSchema(),
+    service = new ShipperService(),
+  ) {
     super(route, service);
   }
+
+  validationsSchema = (req: Request, res: Response, next: NextFunction) => {
+    try {
+      this.schema.validate(req.body);
+      return next();
+    } catch (error) {
+      const { message } = error as Error;
+      return res.status(400).json({ message });
+    }
+  };
 }
 
 export default ShipperController;
--- app/backend/src/routers/ShipperRouters.ts
+++ app/backend/src/routers/ShipperRouters.ts
@@ -3,15 +3,17 @@ import ShipperController from '../controllers/ShipperController';
 
 const shipperController = new ShipperController('/embarcador');
 shipperController.msgNotFound = 'Embarcador não encontrado';
-const { route } = shipperController;
+const { route, validationsSchema } = shipperController;
 const routeId = `${route}/:id`;
 const shipperRouter = Router();
 
 shipperRouter
   .get(routeId, (req, res) => shipperController.findOne(req, res))
-  .put(routeId, (req, res) => shipperController.update(req, res))
+  .put(routeId, validationsSchema, (req, res) => shipperController
+    .update(req, res))
   .delete(routeId, (req, res) => shipperController.delete(req, res))
   .get(route, (req, res) => shipperController.findAll(req, res))
-  .post(route, (req, res) => shipperController.create(req, res));
+  .post(route, validationsSchema, (req, res) => shipperController
+    .create(req, res));
 
 export default shipperRouter;
