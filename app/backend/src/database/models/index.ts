50aef44350ce69308f9b2d76128977fc6e39034d
Matheus Alves <malves224@gmail.com>
2022-04-30 12:58:11 -0300

    feat: (database) model para conveyor (transportador).

---
+++ app/backend/src/database/models/index.ts
@@ -0,0 +1,11 @@
+import Bid from './bid';
+import Conveyor from './conveyor';
+import Offer from './offer';
+import Shipper from './shipper';
+
+export = {
+  Bid,
+  Conveyor,
+  Offer,
+  Shipper,
+};
