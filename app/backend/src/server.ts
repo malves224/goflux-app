001a6b541c4aafd8426bbea7444af6d07c138861
Matheus Alves <malves224@gmail.com>
2022-05-01 14:19:49 -0300

    Feat(Router): add rotas HTTP, para oferta (offer).

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,8 +1,10 @@
 import App from './app';
+import offerRouter from './routers/OfferRouters';
 import shipperRouter from './routers/ShipperRouters';
 
 const server = new App();
 
 server.addRouter(shipperRouter);
+server.addRouter(offerRouter);
 
 export default server;
