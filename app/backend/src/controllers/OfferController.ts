a3a44f86b1c3d14f5cd4ab9946daf1395f5ba62d
Matheus Alves <malves224@gmail.com>
2022-05-01 14:19:17 -0300

    fix(OfferController): corrige instancia de validação de schema.

--- app/backend/src/controllers/OfferController.ts
+++ app/backend/src/controllers/OfferController.ts
@@ -1,13 +1,13 @@
 import Offer from '../database/models/offer';
 import { IOffer } from '../interfaces/Offer';
-import ShipperAndConveyorsSchema from '../schema/ShipperAndConveyors';
+import OfferSchema from '../schema/Offer';
 import OfferService from '../services/OfferService';
 import GenericController from './GenericControllers';
 
 class OfferController extends GenericController<IOffer, Offer> {
   constructor(
     route: string, 
-    schema = new ShipperAndConveyorsSchema().schema,
+    schema = new OfferSchema().schema,
     service = new OfferService(),
   ) {
     super(route, service, schema);
