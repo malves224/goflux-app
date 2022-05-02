a34ecc62f2e82524482d84a1b322fa70bc55c1d2
Matheus Alves <malves224@gmail.com>
2022-05-02 19:54:27 -0300

    feat (script/index): importa funções do localstorage.

--- app/frontend/src/script/index.js
+++ app/frontend/src/script/index.js
@@ -1,3 +1,5 @@
+import storage from './storage';
+
 function formatCnpj(value) {
   return value
     .replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
@@ -17,4 +19,5 @@ export default formatCnpj;
 export {
   checkCnpj,
   sanitizationCnpj,
+  storage,
 };
