43732ed686e3dd3e82ca05b3386dd5815c223a0f
Matheus Alves <malves224@gmail.com>
2022-05-02 18:49:10 -0300

    Feat (Components) criar AlertTogle, um alerta global.

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import AlertTogle from './components/AlertTogle';
+import dataUserContext from './context/Context';
 import Login from './pages/Login';
 
 function App() {
+  const { alertGlobal, setOpenAlert } = useContext(dataUserContext);
+  const { open, severity } = alertGlobal;
   return (
-    <Switch>
-      <Route
-        exact
-        path="/"
-        render={ () => <Login /> }
-      />
-    </Switch>
+    <>
+      <AlertTogle
+        severity={ severity }
+        switchValue={ [open, setOpenAlert] }
+      >
+        {alertGlobal.value}
+      </AlertTogle>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={ () => <Login /> }
+        />
+      </Switch>
+    </>
+
   );
 }
 
