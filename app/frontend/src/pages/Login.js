8d10a4d2db2f82eaaa6f9f8b415b01ce564fe405
Matheus Alves <malves224@gmail.com>
2022-05-02 12:39:25 -0300

    Feat (Login): ao clicar em cadastrar alterna para componente de cadastro.

---
+++ app/frontend/src/pages/Login.js
@@ -0,0 +1,35 @@
+import { Card, Container, Button } from '@mui/material';
+import React, { useState } from 'react';
+import SignIn from '../components/SignIn';
+
+function Login() {
+  const [signingUp, setSigningUp] = useState(false);
+  return (
+    <Container sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 'calc(100vh)',
+      width: '100%',
+    }}
+    >
+      <Card sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '30%',
+        padding: '10px',
+      }}
+      >
+        {signingUp
+          ? <p>cadastro</p>
+          : (<SignIn />) }
+        <Button onClick={() => setSigningUp(!signingUp)} variant="text">
+          {signingUp ? 'Voltar' : 'Cadastar'}
+        </Button>
+
+      </Card>
+    </Container>
+  );
+}
+
+export default Login;
