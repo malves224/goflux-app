a61ea341aa0a16d25c49a330672e162db064e8f7
Matheus Alves <malves224@gmail.com>
2022-05-03 02:14:25 -0300

    Feat (Pages/Shipper): cria pagina embarcador.

--- app/frontend/src/pages/Shipper.js
+++ app/frontend/src/pages/Shipper.js
@@ -1,8 +1,33 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import { Box } from '@mui/material';
+import React, { useState, useEffect, useContext } from 'react';
+import requestApi from '../api';
+import Offers from '../components/Offers';
+import dataUserContext from '../context/Context';
 
 function Shipper() {
+  const [idOffer, setIdOffer] = useState(0);
+  const { offersUser, setOffersUser, userData } = useContext(dataUserContext);
+
+  useEffect(() => {
+    const endpoint = `/Embarcador/${userData.id}`;
+    requestApi(endpoint, 'GET')
+      .then(({ offers }) => setOffersUser(offers));
+  }, [setOffersUser, userData.id]);
+
   return (
-    <div>Shipper</div>
+    <Box
+      sx={ {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+      } }
+    >
+      {idOffer
+        ? (<div>oferta e seus lances</div>)
+        : (<Offers />)}
+    </Box>
   );
 }
 
